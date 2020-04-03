d6861ee5d5fe47d3863f989fd8af165c19e5e6c2
Ghaffaru <mudashiruagm@gmail.com>
2020-04-03 10:36:20 +0000

    visuals

---
+++ mtcars/visuals.R
@@ -0,0 +1,39 @@
+setwd('~/Artificial Intelligence/R-ML-Projects/mtcars')
+
+
+cars <- read.csv('Cars.csv')
+
+
+library(ggplot2)
+
+# create a frequency bar chart
+ggplot(
+  data = cars,
+  aes(x = Transmission)
+) + geom_bar() + ggtitle('Count of cars by Transmission Type') + ylab('Count of Cars')+ xlab('Transmisson Tye')
+
+
+# Histogram
+
+ggplot(
+  data = cars,
+  aes(x = Fuel.Economy)) + 
+  geom_histogram(bins = 10) +
+  ggtitle('Distribution of fuel Economy') + 
+  xlab('Fuel Economy') + 
+  ylab('Count of cars')
+
+# Density plot
+ggplot(
+  data = cars,
+  aes(x = Fuel.Economy)
+) +
+  geom_density() +
+  ggtitle('Distribution of fuel Economy') 
+
+# Create scatter plot
+ggplot(
+  data = cars,
+  aes(x = Cylinders, y = Fuel.Economy)
+) + 
+  geom_point()
